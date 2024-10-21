e995d50dc8638c27193d2e9e30381afca1244670
MarcelFPalianos <mpalia01@student.bbk.ac.uk>
2024-10-21 17:37:19 +0100

    Add files via upload

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+// db.php: Database connection file
+$servername = "localhost";
+$username = "u450382363_admin"; 
+$password = "Marce7f70rin";
+$dbname = "u450382363_users";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
